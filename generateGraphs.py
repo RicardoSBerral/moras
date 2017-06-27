db3ee0370f17aed6e00228c233ce42e5128b7911
Ricardo Sánchez Berral <ricardosanchezberral@outlook.com>
2017-06-27 21:51:24 +0200

    Now plotting number of trees vs MSE.

--- generateGraphs.py
+++ generateGraphs.py
@@ -16,15 +16,23 @@ class DataInfo:
 
 prog = "./nucleo/cons"
 n_partitions = 10
-min_size_forest = 10
-max_size_forest = 100
-step_size_forest = 10
+min_size_forest = 1
+max_size_forest = 6
+step_size_forest = 2
 
 data_wtr = DataInfo("wtr", 22, 5)
 data_cosmo = DataInfo("music2_z0.00_500_1e12", 30, 7)
 
 def call_program (*args):
     return subprocess.Popen(prog + " " + " ".join(str(x) for x in args), shell=True, stdout=subprocess.PIPE).stdout.read()
+
+def call_gnuplot (args_dictionary):
+    args_string = ""
+    for var,value in args_dictionary.iteritems():
+        args_string += "{}='{}';".format(var,value)
+    command = "gnuplot -e \"{}\" plotForestVariableSize.gpi".format(args_string)
+    print command
+    return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
 
 def get_num_nodes (str):
     m = re.search("Num nodos: (\d+) ", str)
@@ -49,12 +57,12 @@ def get_results_multi (dataInfo, ret, matrix):
             matrix[i].append(float(m.group(1)))
     return matrix
 
-def get_results_multi_variable_trees_number (dataInfo, ret, matrix, size):
+def get_results_multi_variable_trees_number (dataInfo, ret, matrix, size_index):
     lines = ret.splitlines()[-dataInfo.number_objectives-1:-1]
     for i in xrange(0, dataInfo.number_objectives):
         m = re.match("\d+: ([\.\d]+)", lines[i])
         if m:
-            matrix[i][size].append(float(m.group(1)))
+            matrix[i][size_index].append(float(m.group(1)))
     return matrix
 
 def single_objective_tree(dataInfo):
@@ -104,9 +112,9 @@ def multi_objective_forest_fixed_trees_number(dataInfo):
 
 def single_objective_forest_variable_trees_number(dataInfo):
     for i in xrange(dataInfo.first_variable, dataInfo.n_variables):
-        with open("{}_{:d}_variable.csv".format(dataInfo.data, i), 'wb') as csvfile:
+        with open("{}_{:d}_variable_single.csv".format(dataInfo.data, i), 'wb') as csvfile:
             csvwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            for s in xrange(min_size_forest, max_size_forest, step_size_forest):
+            for s in xrange(min_size_forest, max_size_forest + 1, step_size_forest):
                 partial_results = []
                 for j in xrange(0, n_partitions):
                     train_partition = dataInfo.data + "_train_cv" + str(j)
@@ -117,19 +125,28 @@ def single_objective_forest_variable_trees_number(dataInfo):
                 csvwriter.writerow([s, average, sd])
 
 def multi_objective_forest_variable_trees_number(dataInfo):
-    matrix = [[[] for y in xrange(min_size_forest, max_size_forest, step_size_forest)] for x in xrange(0, dataInfo.number_objectives)]
-    for s in xrange(min_size_forest, max_size_forest, step_size_forest):
+    tree_size_index = list(enumerate(xrange(min_size_forest, max_size_forest + 1, step_size_forest)))
+    matrix = [[[] for s in tree_size_index] for x in xrange(0, dataInfo.number_objectives)]
+    for index_size, size in tree_size_index:
         for j in xrange(0, n_partitions):
             train_partition = dataInfo.data + "_train_cv" + str(j)
             test_partition = dataInfo.data + "_test_cv" + str(j)
-            ret = call_program("testMultiObjectiveForest.mKo", train_partition, test_partition, dataInfo.first_variable, dataInfo.number_objectives, s)
-            matrix = get_results_multi_variable_trees_number(dataInfo, ret, matrix, s)
+            ret = call_program("testMultiObjectiveForest.mKo", train_partition, test_partition, dataInfo.first_variable, dataInfo.number_objectives, size)
+            matrix = get_results_multi_variable_trees_number(dataInfo, ret, matrix, index_size)
     for i in xrange(0, dataInfo.number_objectives):
-        with open("{}_{:d}_variable.csv".format(dataInfo.data, i), 'wb') as csvfile:
+        with open("{}_{:d}_variable_multi.csv".format(dataInfo.data, i + dataInfo.first_variable), 'wb') as csvfile:
             csvwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            for s in xrange(min_size_forest, max_size_forest, step_size_forest):
-                average, sd = average_and_sd(matrix[i][s])
-                csvwriter.writerow([s, average, sd])
+            for index_size, size in tree_size_index:
+                average, sd = average_and_sd(matrix[i][index_size])
+                csvwriter.writerow([size, average, sd])
+
+def generate_graphs(dataInfo):
+    for i in xrange(dataInfo.first_variable, dataInfo.n_variables):
+        call_gnuplot({
+            "filename": "{}_{:d}_variable.png".format(dataInfo.data, i),
+            "data": dataInfo.data,
+            "variable": str(i),
+        })
 
 def main():
 
@@ -141,42 +158,48 @@ def main():
     print "\n***** WTR *****"
 
     print "\nSINGLE-OBJECTIVE TREE"
-    # single_objective_tree(data_wtr)
+    single_objective_tree(data_wtr)
 
     print "\nMULTI-OBJECTIVE TREE"
-    # multi_objective_tree(data_wtr)
+    multi_objective_tree(data_wtr)
 
     print "\nSINGLE-OBJECTIVE FOREST WITH 10 TREES"
-    # single_objective_forest_fixed_trees_number(data_wtr)
+    single_objective_forest_fixed_trees_number(data_wtr)
 
     print "\nMULTI-OBJECTIVE FOREST WITH 10 TREES"
-    # multi_objective_forest_fixed_trees_number(data_wtr)
+    multi_objective_forest_fixed_trees_number(data_wtr)
 
     print "\nSINGLE-OBJECTIVE FOREST WITH VARIABLE TREES"
     single_objective_forest_variable_trees_number(data_wtr)
 
-    print "\nMULTI-OBJECTIVE FOREST WITH 10 TREES"
+    print "\nMULTI-OBJECTIVE FOREST WITH VARIABLE TREES"
     multi_objective_forest_variable_trees_number(data_wtr)
+
+    print "\nGENERATE GRAPHS"
+    generate_graphs(data_wtr)
 
     print "\n***** COSMO *****"
 
     print "\nSINGLE-OBJECTIVE TREE"
-    # single_objective_tree(data_cosmo)
+    single_objective_tree(data_cosmo)
 
     print "\nMULTI-OBJECTIVE TREE"
-    # multi_objective_tree(data_cosmo)
+    multi_objective_tree(data_cosmo)
 
     print "\nSINGLE-OBJECTIVE FOREST WITH 10 TREES"
-    # single_objective_forest_fixed_trees_number(data_cosmo)
+    single_objective_forest_fixed_trees_number(data_cosmo)
 
     print "\nMULTI-OBJECTIVE FOREST WITH 10 TREES"
-    # multi_objective_forest_fixed_trees_number(data_cosmo)
+    multi_objective_forest_fixed_trees_number(data_cosmo)
 
     print "\nSINGLE-OBJECTIVE FOREST WITH VARIABLE TREES"
     single_objective_forest_variable_trees_number(data_cosmo)
 
-    print "\nMULTI-OBJECTIVE FOREST WITH 10 TREES"
+    print "\nMULTI-OBJECTIVE FOREST WITH VARIABLE TREES"
     multi_objective_forest_variable_trees_number(data_cosmo)
+
+    print "\nGENERATE GRAPHS"
+    generate_graphs(data_cosmo)
 
     # Para cada ejemplo, Classify
     # ErrSecClass
