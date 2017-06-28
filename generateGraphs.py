b56eea78ef9a838fc1ceff776073485edd604287
Ricardo Sánchez Berral <ricardosanchezberral@outlook.com>
2017-06-28 07:01:02 +0200

    Changed sizes of forests.

--- generateGraphs.py
+++ generateGraphs.py
@@ -16,9 +16,9 @@ class DataInfo:
 
 prog = "./nucleo/cons"
 n_partitions = 10
-min_size_forest = 1
-max_size_forest = 6
-step_size_forest = 2
+min_size_forest = 10
+max_size_forest = 100
+step_size_forest = 10
 
 data_wtr = DataInfo("wtr", 22, 5)
 data_cosmo = DataInfo("music2_z0.00_500_1e12", 30, 7)
@@ -31,7 +31,6 @@ def call_gnuplot (args_dictionary):
     for var,value in args_dictionary.iteritems():
         args_string += "{}='{}';".format(var,value)
     command = "gnuplot -e \"{}\" plotForestVariableSize.gpi".format(args_string)
-    print command
     return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
 
 def get_num_nodes (str):
@@ -49,102 +48,102 @@ def average_with_sd (list):
     average, sd = average_and_sd (list)
     return "{:.2E} ± {:.3E}".format(Decimal(average), Decimal(sd))
 
-def get_results_multi (dataInfo, ret, matrix):
-    lines = ret.splitlines()[-dataInfo.number_objectives-1:-1]
-    for i in xrange(0, dataInfo.number_objectives):
+def get_results_multi (data_info, ret, matrix):
+    lines = ret.splitlines()[-data_info.number_objectives-1:-1]
+    for i in xrange(0, data_info.number_objectives):
         m = re.match("\d+: ([\.\d]+)", lines[i])
         if m:
             matrix[i].append(float(m.group(1)))
     return matrix
 
-def get_results_multi_variable_trees_number (dataInfo, ret, matrix, size_index):
-    lines = ret.splitlines()[-dataInfo.number_objectives-1:-1]
-    for i in xrange(0, dataInfo.number_objectives):
+def get_results_multi_variable_trees_number (data_info, ret, matrix, size_index):
+    lines = ret.splitlines()[-data_info.number_objectives-1:-1]
+    for i in xrange(0, data_info.number_objectives):
         m = re.match("\d+: ([\.\d]+)", lines[i])
         if m:
             matrix[i][size_index].append(float(m.group(1)))
     return matrix
 
-def single_objective_tree(dataInfo):
-    for i in xrange(dataInfo.first_variable, dataInfo.n_variables):
+def single_objective_tree(data_info):
+    for i in xrange(data_info.first_variable, data_info.n_variables):
         partial_results = []
         partial_nodes = []
         for j in xrange(0, n_partitions):
-            train_partition = dataInfo.data + "_train_cv" + str(j)
-            test_partition = dataInfo.data + "_test_cv" + str(j)
+            train_partition = data_info.data + "_train_cv" + str(j)
+            test_partition = data_info.data + "_test_cv" + str(j)
             ret = call_program("testSingleObjectiveTree.mKo", train_partition, test_partition, i)
             partial_results.append(float(ret.splitlines()[-1]))
             partial_nodes.append(get_num_nodes(ret))
         print "{:d}º variable: {}, nodes: {}".format(i, average_with_sd(partial_results), average_with_sd(partial_nodes)) 
 
-def multi_objective_tree(dataInfo):
-    matrix = [[] for x in xrange(0, dataInfo.number_objectives)]
+def multi_objective_tree(data_info):
+    matrix = [[] for x in xrange(0, data_info.number_objectives)]
     nodes = []
     for j in xrange(0, n_partitions):
-        train_partition = dataInfo.data + "_train_cv" + str(j)
-        test_partition = dataInfo.data + "_test_cv" + str(j)
-        ret = call_program("testMultiObjectiveTree.mKo", train_partition, test_partition, dataInfo.first_variable, dataInfo.number_objectives)
-        matrix = get_results_multi(dataInfo, ret, matrix)
+        train_partition = data_info.data + "_train_cv" + str(j)
+        test_partition = data_info.data + "_test_cv" + str(j)
+        ret = call_program("testMultiObjectiveTree.mKo", train_partition, test_partition, data_info.first_variable, data_info.number_objectives)
+        matrix = get_results_multi(data_info, ret, matrix)
         nodes.append(get_num_nodes(ret))
-    for i in xrange(0, dataInfo.number_objectives):
-        print "{:d}º variable: {}".format(i + dataInfo.first_variable, average_with_sd(matrix[i]))
+    for i in xrange(0, data_info.number_objectives):
+        print "{:d}º variable: {}".format(i + data_info.first_variable, average_with_sd(matrix[i]))
     print "Nodes: {}".format(average_with_sd(nodes))
 
-def single_objective_forest_fixed_trees_number(dataInfo):
-    for i in xrange(dataInfo.first_variable, dataInfo.n_variables):
+def single_objective_forest_fixed_trees_number(data_info, size_forest = 10):
+    for i in xrange(data_info.first_variable, data_info.n_variables):
         partial_results = []
         for j in xrange(0, n_partitions):
-            train_partition = dataInfo.data + "_train_cv" + str(j)
-            test_partition = dataInfo.data + "_test_cv" + str(j)
-            ret = call_program("testSingleObjectiveForest.mKo", train_partition, test_partition, i, 10)
+            train_partition = data_info.data + "_train_cv" + str(j)
+            test_partition = data_info.data + "_test_cv" + str(j)
+            ret = call_program("testSingleObjectiveForest.mKo", train_partition, test_partition, i, size_forest)
             partial_results.append(float(ret.splitlines()[-1]))
         print "{:d}º variable: {}".format(i, average_with_sd(partial_results))
 
-def multi_objective_forest_fixed_trees_number(dataInfo):
-    matrix = [[] for x in xrange(0, dataInfo.number_objectives)]
+def multi_objective_forest_fixed_trees_number(data_info, size_forest = 10):
+    matrix = [[] for x in xrange(0, data_info.number_objectives)]
     for j in xrange(0, n_partitions):
-        train_partition = dataInfo.data + "_train_cv" + str(j)
-        test_partition = dataInfo.data + "_test_cv" + str(j)
-        ret = call_program("testMultiObjectiveForest.mKo", train_partition, test_partition, dataInfo.first_variable, dataInfo.number_objectives, 10)
-        matrix = get_results_multi(dataInfo, ret, matrix)
-    for i in xrange(0, dataInfo.number_objectives):
-        print "{:d}º variable: {}".format(i + dataInfo.first_variable, average_with_sd(matrix[i]))
+        train_partition = data_info.data + "_train_cv" + str(j)
+        test_partition = data_info.data + "_test_cv" + str(j)
+        ret = call_program("testMultiObjectiveForest.mKo", train_partition, test_partition, data_info.first_variable, data_info.number_objectives, size_forest)
+        matrix = get_results_multi(data_info, ret, matrix)
+    for i in xrange(0, data_info.number_objectives):
+        print "{:d}º variable: {}".format(i + data_info.first_variable, average_with_sd(matrix[i]))
 
-def single_objective_forest_variable_trees_number(dataInfo):
-    for i in xrange(dataInfo.first_variable, dataInfo.n_variables):
-        with open("{}_{:d}_variable_single.csv".format(dataInfo.data, i), 'wb') as csvfile:
+def single_objective_forest_variable_trees_number(data_info):
+    for i in xrange(data_info.first_variable, data_info.n_variables):
+        with open("{}_{:d}_variable_single.csv".format(data_info.data, i), 'wb') as csvfile:
             csvwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
             for s in xrange(min_size_forest, max_size_forest + 1, step_size_forest):
                 partial_results = []
                 for j in xrange(0, n_partitions):
-                    train_partition = dataInfo.data + "_train_cv" + str(j)
-                    test_partition = dataInfo.data + "_test_cv" + str(j)
+                    train_partition = data_info.data + "_train_cv" + str(j)
+                    test_partition = data_info.data + "_test_cv" + str(j)
                     ret = call_program("testSingleObjectiveForest.mKo", train_partition, test_partition, i, s)
                     partial_results.append(float(ret.splitlines()[-1]))
                 average, sd = average_and_sd(partial_results)
                 csvwriter.writerow([s, average, sd])
 
-def multi_objective_forest_variable_trees_number(dataInfo):
+def multi_objective_forest_variable_trees_number(data_info):
     tree_size_index = list(enumerate(xrange(min_size_forest, max_size_forest + 1, step_size_forest)))
-    matrix = [[[] for s in tree_size_index] for x in xrange(0, dataInfo.number_objectives)]
+    matrix = [[[] for s in tree_size_index] for x in xrange(0, data_info.number_objectives)]
     for index_size, size in tree_size_index:
         for j in xrange(0, n_partitions):
-            train_partition = dataInfo.data + "_train_cv" + str(j)
-            test_partition = dataInfo.data + "_test_cv" + str(j)
-            ret = call_program("testMultiObjectiveForest.mKo", train_partition, test_partition, dataInfo.first_variable, dataInfo.number_objectives, size)
-            matrix = get_results_multi_variable_trees_number(dataInfo, ret, matrix, index_size)
-    for i in xrange(0, dataInfo.number_objectives):
-        with open("{}_{:d}_variable_multi.csv".format(dataInfo.data, i + dataInfo.first_variable), 'wb') as csvfile:
+            train_partition = data_info.data + "_train_cv" + str(j)
+            test_partition = data_info.data + "_test_cv" + str(j)
+            ret = call_program("testMultiObjectiveForest.mKo", train_partition, test_partition, data_info.first_variable, data_info.number_objectives, size)
+            matrix = get_results_multi_variable_trees_number(data_info, ret, matrix, index_size)
+    for i in xrange(0, data_info.number_objectives):
+        with open("{}_{:d}_variable_multi.csv".format(data_info.data, i + data_info.first_variable), 'wb') as csvfile:
             csvwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
             for index_size, size in tree_size_index:
                 average, sd = average_and_sd(matrix[i][index_size])
                 csvwriter.writerow([size, average, sd])
 
-def generate_graphs(dataInfo):
-    for i in xrange(dataInfo.first_variable, dataInfo.n_variables):
+def generate_graphs(data_info):
+    for i in xrange(data_info.first_variable, data_info.n_variables):
         call_gnuplot({
-            "filename": "{}_{:d}_variable.png".format(dataInfo.data, i),
-            "data": dataInfo.data,
+            "filename": "{}_{:d}_variable.png".format(data_info.data, i),
+            "data": data_info.data,
             "variable": str(i),
         })
 
@@ -169,6 +168,12 @@ def main():
     print "\nMULTI-OBJECTIVE FOREST WITH 10 TREES"
     multi_objective_forest_fixed_trees_number(data_wtr)
 
+    print "\nSINGLE-OBJECTIVE FOREST WITH 100 TREES"
+    single_objective_forest_fixed_trees_number(data_wtr, 100)
+
+    print "\nMULTI-OBJECTIVE FOREST WITH 100 TREES"
+    multi_objective_forest_fixed_trees_number(data_wtr, 100)
+
     print "\nSINGLE-OBJECTIVE FOREST WITH VARIABLE TREES"
     single_objective_forest_variable_trees_number(data_wtr)
 
@@ -191,6 +196,12 @@ def main():
 
     print "\nMULTI-OBJECTIVE FOREST WITH 10 TREES"
     multi_objective_forest_fixed_trees_number(data_cosmo)
+
+    print "\nSINGLE-OBJECTIVE FOREST WITH 100 TREES"
+    single_objective_forest_fixed_trees_number(data_cosmo, 100)
+
+    print "\nMULTI-OBJECTIVE FOREST WITH 100 TREES"
+    multi_objective_forest_fixed_trees_number(data_cosmo, 100)
 
     print "\nSINGLE-OBJECTIVE FOREST WITH VARIABLE TREES"
     single_objective_forest_variable_trees_number(data_cosmo)
